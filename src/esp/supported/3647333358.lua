e6f1577c2350e74f718162c4a33c878cd992f865
daximul <79027135+daximul@users.noreply.github.com>
2023-02-10 13:01:13 -0500

    Update 3647333358.lua

--- src/esp/supported/3647333358.lua
+++ src/esp/supported/3647333358.lua
@@ -4,6 +4,19 @@
 ]]
 
 return function(Container, Section, esp)
+    esp:AddObjectListener(workspace, {
+        Type = "Model",
+        Recursive = true,
+        PrimaryPart = "HumanoidRootPart",
+        CustomName = function(obj)
+            return "[AI] " .. tostring(obj.Name)
+        end,
+        Color = esp.Presets.Red,
+        Validator = function(obj)
+            return obj:GetAttribute("AI") == true
+        end,
+        IsEnabled = "Nextbots"
+    })
     esp.Overrides.GetColor = function(character)
         local player = esp:GetPlrFromChar(character)
         if player and esp.Downed and player.Character:GetAttribute("Downed") == true then
@@ -11,5 +24,6 @@ return function(Container, Section, esp)
         end
         return esp.Color
     end
+    Section:AddItem("Toggle", {Text = "Nextbots", Function = function(callback) esp.Nextbots = callback end})
     Section:AddItem("Toggle", {Text = "Downed", Function = function(callback) esp.Downed = callback end})
 end
