89e6a7aaa78544f39eb5982f4d35538f3e6ffeca
daximul <79027135+daximul@users.noreply.github.com>
2023-02-14 23:17:03 -0500

    Update shadermod.lua

--- src/browser/shadermod.lua
+++ src/browser/shadermod.lua
@@ -1,5 +1,5 @@
 local Blur = NewInstance("BlurEffect", {Name = RandomString(), Parent = Services.Lighting, Size = 4, Enabled = false})
-
+local Depth = NewInstance("DepthOfFieldEffect", {Name = RandomString(), Parent = Services.Lighting, FarIntensity = 0.75, FocusDistance = 0.05, InFocusRadius = 10, NearIntensity = 0.75, Enabled = false})
 return {
 	Name = "Shader Mod",
 	Commands = {
@@ -7,7 +7,7 @@ return {
 			Name = "toggleshader",
 			Description = "Toggles shaders."
 			Function = function()
-				-- code
+				Blur.Enabled, Depth.Enabled = not Blur.Enabled, not Depth.Enabled
 			end
 		}
 	}
