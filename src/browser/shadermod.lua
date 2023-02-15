735b896d6a0cc4d6b0805d4544b849a8204e75ed
daximul <79027135+daximul@users.noreply.github.com>
2023-02-14 23:14:08 -0500

    Update shadermod.lua

--- src/browser/shadermod.lua
+++ src/browser/shadermod.lua
@@ -1,1 +1,14 @@
-return {Name = "shador modz", Description = "testing", Commands = {}}
+local Blur = NewInstance("BlurEffect", {Name = RandomString(), Parent = Services.Lighting, Size = 4, Enabled = false})
+
+return {
+	Name = "Shader Mod",
+	Commands = {
+		{
+			Name = "toggleshader",
+			Description = "Toggles shaders."
+			Function = function()
+				-- code
+			end
+		}
+	}
+}
