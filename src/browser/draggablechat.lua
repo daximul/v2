ff33c8c413bad00ec1e274a9b16d0143b9a2a61a
daximul <79027135+daximul@users.noreply.github.com>
2023-02-27 21:07:53 -0500

    Create draggablechat.lua

---
+++ src/browser/draggablechat.lua
@@ -0,0 +1,19 @@
+return {
+    Name = "Draggable Chat",
+    Commands = {
+        {
+            Name = "draggablechat",
+            Description = "Makes chat draggable and resizable.",
+            Aliases = {"dragchat"},
+            Category = "Utility",
+            Function = function()
+                local ChatSettings = require(Services.Chat.ClientChatModules.ChatSettings)
+                local ChatFrame = Players.LocalPlayer:FindFirstChildWhichIsA("PlayerGui").Chat.Frame
+                ChatSettings.WindowResizable = true
+                ChatSettings.WindowDraggable = true
+                ChatFrame.ChatChannelParentFrame.Visible = true
+                ChatFrame.ChatBarParentFrame.Position = ChatFrame.ChatChannelParentFrame.Position + UDim2.new(UDim.new(), ChatFrame.ChatChannelParentFrame.Size.Y)
+            end
+        }
+    }
+}
